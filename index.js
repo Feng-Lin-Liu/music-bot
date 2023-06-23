933a8674ce6ca6c33b2101bbbfc7f614a398cb39
Henry Liu <113130071+Feng-Lin-Liu@users.noreply.github.com>
2023-06-23 10:14:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {
     prefix,
     token,
 } = require("./config.json");
-const ytdl = require("ytdl-core-discord");
+const ytdl = require("ytdl-core");
 
 const client = new Discord.Client();
 const queue = new Map();
